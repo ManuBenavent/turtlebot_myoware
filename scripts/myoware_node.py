9612037a59effb5b093c930ef31840bca3a1e600
ManuBenavent <mbenavent99@gmail.com>
2022-05-04 12:42:35 +0200

    version basica

---
+++ scripts/myoware_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import UInt32
+from myoware_constants import *
+
+import select, sys
+
+if __name__ == "__main__":
+    rospy.init_node('myoware_node')
+    pub = rospy.Publisher('/myoware_signal', UInt32, queue_size=5)
+
+    while True:
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if rlist:
+            try:
+                key = int(sys.stdin.read(1))
+                if key not in range(4):
+                    key = STOP
+            except:
+                key = STOP
+        else:
+            key = STOP
+        pub.publish(2)
+    
+    
---
+++ scripts/turtlebot_teleop_myoware.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import UInt32
+from myoware_constants import *
+import select, sys
+
+
+def callback(data):
+    data = data.data
+    twist = Twist()
+    try:
+        if data == STOP:
+            twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif data == MOVE_FORWARD:
+            twist.linear.x = 0.2; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif data == TURN_LEFT:
+            twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 1; twist.angular.y = 0; twist.angular.z = 0
+        elif data == TURN_RIGHT:
+            twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = -1; twist.angular.y = 0; twist.angular.z = 0
+        pub.publish(twist)
+    except:
+        rospy.logfatal("Se ha producido una excepcion. Robot detenido.")
+
+    finally:
+        # Por seguridad, si hay cualquier fallo ponemos velocidades a 0
+        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        pub.publish(twist)
+    
+
+if __name__=="__main__":
+    rospy.init_node('turtlebot_teleop_myoware')
+    pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=5)
+    rospy.Subscriber('/myoware_signal', UInt32, callback, queue_size=5)
+
+    try:
+        rospy.spin()
+            
+    except:
+        rospy.logfatal("Se ha producido una excepcion. Robot detenido.")
+
+    finally:
+        # Por seguridad, si hay cualquier fallo ponemos velocidades a 0
+        twist = Twist()
+        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        pub.publish(twist)
+
