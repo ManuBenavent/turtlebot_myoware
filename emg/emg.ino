41029d14053ef003461d04e6d8a3bed4444759b7
ManuBenavent <mbenavent99@gmail.com>
2022-05-09 19:12:42 +0200

    myoware

---
+++ emg/emg.ino
@@ -0,0 +1,45 @@
+const int potPinUno = 34;
+const int potPinDos = 35;
+int EMGValue1 = 0;
+int EMGValue2 = 0;
+
+// The setup routine runs once when you press reset:
+void setup() {
+  // Initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+}
+
+// The loop routine runs over and over again forever:
+void loop() {
+
+  EMGValue1 = analogRead(potPinUno);
+  EMGValue2 = analogRead(potPinDos);
+  // Write the signal points, followed by the terminator "Carriage Return" and "Linefeed".
+  //Serial.print("0");
+  Serial.print(EMGValue1);
+  Serial.print(" ");
+  //Serial.print("1");
+  Serial.println(EMGValue2);
+  //Serial.write(13);
+  //Serial.write(10);
+}
+
+
+/*// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
+
+
+// variable for storing the potentiometer value
+int potValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  // Reading potentiometer value
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(50);
+}*/
--- scripts/myoware_node.py
+++ scripts/myoware_node.py
@@ -3,24 +3,68 @@
 import rospy
 from std_msgs.msg import UInt32
 from myoware_constants import *
+import serial, time
+import re
 
 import select, sys
+umbral1=4000
+umbral2=4000
+
+arduino = serial.Serial('COM4', 9600)
+time.sleep(2)
+rawString = arduino.readline()
+flexion = 0
+extension = 0
+cont=0
+valor_anterior_f=0
+valor_anterior_e=0
 
 if __name__ == "__main__":
     rospy.init_node('myoware_node')
     pub = rospy.Publisher('/myoware_signal', UInt32, queue_size=5)
 
     while True:
-        rlist, _, _ = select.select([sys.stdin], [], [], 5)
-        if rlist:
-            try:
-                key = int(sys.stdin.read(1))
-                if key not in range(4):
-                    key = STOP
-            except:
-                key = STOP
+        # rlist, _, _ = select.select([sys.stdin], [], [], 5)
+        # if rlist:
+        #     try:
+        #         key = int(sys.stdin.read(1))
+        #         if key not in range(4):
+        #             key = STOP
+        #     except:
+        #         key = STOP
+        # else:
+        #     key = STOP
+        # pub.publish(key)
+
+        rawString = arduino.readline()
+        valor = rawString.decode("utf-8")
+        
+        
+        valores=valor.split()
+        # print("0: ",valores[0])
+        # print("1: ",valores[1])
+
+        
+        # if(valor[0] == 0):
+            #for i in range(2, len(valor)):
+
+            # flexion = flexion[1:]
+            
+        # elif(valor[0] == 1):
+            # extension = valor[1:]
+
+        #time.sleep(0.05)
+        # print("valor: ", valor)
+
+        if int(valores[0])>umbral1 and int(valores[1])<umbral2:
+                command=TURN_LEFT
+        elif int(valores[0])<umbral1 and int(valores[1])>umbral2:
+                command=TURN_RIGHT
+        elif int(valores[0])>umbral1 and int(valores[1])>umbral2:
+                command=MOVE_FORWARD
         else:
-            key = STOP
-        pub.publish(key)
-    
+                command=STOP
+                
+        print ("estado: ", command)
+        pub.publish(command)
     
